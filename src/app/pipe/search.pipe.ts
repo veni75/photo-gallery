967eadf5ea67a7a9e815557c0cc43b40a204e615
Várdai Enikő <vardai.eniko@gmail.com>
2021-03-16 12:15:53 +0100

    search

--- src/app/pipe/search.pipe.ts
+++ src/app/pipe/search.pipe.ts
@@ -5,13 +5,24 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchPipe implements PipeTransform {
 
-  transform(value: any[], key: string): any[] {
+  transform(value: any[]|null, key: string, gender:string): any[]|null {
     if (!Array.isArray(value) || !key) {
       return value;
     }
 
-    return value
-      .filter(item => (item[key]));
+    if (key === "liked" || key === "accepted") {
+      return value
+        .filter(item => (item[key]));
+    } else if (key === "interests") {
+      return value
+        .filter(item => (item[key]) === "Angular" );
+    } else if (key === "location") {
+      return value
+        .filter(item => (item[key]) === "Budapest");
+    }else if(key === "gender") {
+      return value
+        .filter(item => (item[key]) === gender);
+    }
 
   };
 
